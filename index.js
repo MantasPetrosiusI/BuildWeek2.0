dea6344b9b83eb0d66d368cf817e99c17dd69833
MantasPetrosiusI <116674326+MantasPetrosiusI@users.noreply.github.com>
2023-01-26 00:46:17 +0000

    spotifyPlayer

--- index.js
+++ index.js
@@ -10,33 +10,89 @@ const options = {
   },
 };
 
-const fetchSong = async() =>{
+const audioPlayer = async() =>{
     const res = await fetch(url+"Metallica", options)
     const songs = await res.json();
     console.log(songs)
 
+    //CHANGE EVERYTHING THATS HARDCODED ACCORDINGLY TO OTHER JS FILES TO CORRESPOND TO THEIR FETCH/ onClick e.g songs.data[14]
+    let audio = new Audio(`${songs.data[14].preview}`)
+    
+    
+    const play = document.querySelector('.play.fa-solid.fa-circle-play');
+    const pause = document.querySelector('.pause.fa-solid.fa-circle-pause');
+    const loop = document.querySelector('.fa-solid.fa-repeat');
+    const shuffle = document.querySelector('.fas.fa-random')
 
-    const playPauses = document.querySelectorAll('.play-pause.fas.fa-play');
-    let state = "paused";
-        const audio = new Audio(`${songs.data[3].preview}`)
-        playPauses.forEach((playPause)=>{
-        playPause.addEventListener('click', ()=>{
-            if(state === "paused"){
-                audio.play()
-                state = "playing"
-            }else{
+    shuffle.addEventListener('click', ()=>{
+                let randomSong = songs.data[Math.floor(Math.random()*songs.data.length)].preview
                 audio.pause()
+                audio = new Audio(`${randomSong}`).play()
+            })
+    document.querySelector('.image-container').innerHTML +=`<img src="${songs.data[14].album.cover_small}" alt=""/>`
+    document.querySelector('p.title').innerHTML += `<b>${songs.data[14].album.title}</b>`;
+    document.querySelector('p.artist').innerHTML += `${songs.data[14].artist.name}`;
+    
+    let state = "paused";
+        play.addEventListener('click', ()=>{
+            if(state === "paused"){
+                audio.play()            
+                play.style.display = "none";
+                pause.style.display= "block";
+                state = "playing"
+
+                
+            }
+        pause.addEventListener('click', () =>{
+            if(state = "playing"){
+
+                audio.pause()
+                play.style.display= "block";
+                pause.style.display ="none";
                 state = "paused"
+            }     
+            
+        })
+        
+            
+        })
+        let isLooping = false;
+        loop.addEventListener('click', ()=>{
+            
+            if(!isLooping){
+                audio.loop = true;
+                isLooping = true;   
+                loop.style.color = "#ddd"       
+            }else{
+                audio.loop = false;
+                isLooping = false;
+                loop.style.color = "#999"
             }
             
         })
-        })
-}
-fetchSong()
 
 
+      songCurrent = setInterval(function() {
+      let current = document.getElementById("current")
+      let mins = Math.floor(audio.currentTime / 60);
+      let secs = Math.floor(audio.currentTime % 60);
+      if (secs < 10) {
+        secs = '0' + String(secs);
+      }
+      current.innerHTML = mins + ':' + secs;
+    }, 10);
 
-
-
-
-
+    audio.addEventListener('loadeddata', ()=>{
+        let songLength = document.getElementById("length")
+        let mins = Math.floor(audio.duration / 60);
+        let secs = Math.floor(audio.duration % 60);
+        if (secs < 10) {
+        secs = '0' + String(secs);
+      }
+      songLength.innerHTML = mins + ':' + secs;
+    })
+        }
+    let audioHtml = document.querySelector('#audio');
+    let dur = document.querySelector('#dur')
+    
+window.onload = async ()=> {await audioPlayer()}
