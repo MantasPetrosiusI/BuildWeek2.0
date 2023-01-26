a8f95705d4ffc7ccfc2b5ea4fbf81fcfe02eb426
MantasPetrosiusI <116674326+MantasPetrosiusI@users.noreply.github.com>
2023-01-26 18:40:18 +0000

    vol

--- player/player.js
+++ player/player.js
@@ -11,15 +11,15 @@ const playerOptions = {
 };
 
 const audioPlayer = async (albumId) => {
+
+
     let respons = await fetch(` https://striveschool-api.herokuapp.com/api/deezer/album/${albumId}`, playerOptions)
     let data = await respons.json();
     const songs = data.tracks
     let arraySongs = []
     songs.data.map((song)=>{
         arraySongs.push(song)
-        console.log(song)
     })
-    console.log(songs.data)
     let i = 0;
 
     const play = document.querySelector('.play.fa-solid.fa-circle-play');
@@ -28,41 +28,46 @@ const audioPlayer = async (albumId) => {
     const next = document.querySelector('.fas.fa-step-forward');
     const loop = document.querySelector('.fa-solid.fa-repeat');
     const shuffle = document.querySelector('.fas.fa-random');
+    const unmute = document.querySelector('.fas.fa-volume-down')
+    const mute = document.querySelector('.fa-solid.fa-volume-xmark')
 
     audio = document.querySelector('audio')
-    audio.play();
-    audio.src =  `${songs.data[i].preview}`;
     
-
+    audio.src =  `${songs.data[i].preview}`;
+    audio.volume = 0.5;
+    audio.play();
+    play.style.display = "none";
+    pause.style.display = "block";
+    
+    //SHUFFLE
     shuffle.addEventListener('click', () => {
-        let randomSong = songs.data[Math.floor(Math.random() * songs.data.length)].preview
+
+        i = Math.floor(Math.random() * songs.data.length);
+        let randomSong = songs.data[i].preview
         audio.src = `${randomSong}`
         audio.play()
     })
     document.querySelector('.image-container').innerHTML = `<img src="${songs.data[i].album.cover_small}" alt=""/>`
     document.querySelector('p.title').innerHTML = `<b>${songs.data[i].album.title}</b>`;
     document.querySelector('p.artist').innerHTML = `${songs.data[i].artist.name}`;
-    console.log(audio.paused)
+
+    //PLAY
     play.addEventListener('click', () => {
         if (audio.paused) {
             audio.play()
             play.style.display = "none";
             pause.style.display = "block";
-            state = "playing"
-
-
         }
-        pause.addEventListener('click', () => {
-            if (state = "playing") {
-
-                audio.pause()
-                play.style.display = "block";
-                pause.style.display = "none";
-                state = "paused"
-            }
-
-        })
     })
+    //PAUSE
+    pause.addEventListener('click', () => {
+        if (!audio.paused) {
+            audio.pause()
+            play.style.display = "block";
+            pause.style.display = "none";
+        }
+        })
+    //REPEAT
     let isLooping = false;
     loop.addEventListener('click', () => {
 
@@ -77,10 +82,14 @@ const audioPlayer = async (albumId) => {
         }
 
     })
+    //TIMER =>>
     songCurrent = setInterval(function () {
-        let current = document.getElementById("current")
+        let current = document.getElementById("current");
+        let bar = document.querySelector('.progress');
         let mins = Math.floor(audio.currentTime / 60);
         let secs = Math.floor(audio.currentTime % 60);
+        let calc = Math.trunc((100*secs)/(audio.duration%60));
+        bar.style.width = `${calc}%`
         if (secs < 10) {
             secs = '0' + String(secs);
         }
@@ -96,20 +105,20 @@ const audioPlayer = async (albumId) => {
         }
         songLength.innerHTML = mins + ':' + secs;
     })
+    // <<=
+    //PREVIOUS SONG
     prev.addEventListener('click', () =>{
         if(i===0){
             i = arraySongs.length-1;
             audio.src = `${songs.data[i].preview}`
-            audio.play()
-            
+            audio.play()       
         }else{
             i--;
             audio.src = `${songs.data[i].preview}`
             audio.play()
-            
-        }
-        
+        }  
     })
+    //NEXT SONG
     next.addEventListener('click', () =>{
         if(i === arraySongs.length){
             i = 0;
@@ -121,5 +130,38 @@ const audioPlayer = async (albumId) => {
             audio.play()
         }
     })
+
+    //AFTER ONE SONG IS DONE NEXT ON PLAYS
+    audio.onended = () =>{
+        if(i === arraySongs.length){
+            i = 0;
+            audio.src = audio.src = `${songs.data[i].preview}`
+            audio.play()
+        }else{
+            i++;
+            audio.src = `${songs.data[i].preview}`
+            audio.play()
+        }
+    }
+    //MUTE
+    mute.addEventListener('click', () =>{
+        if(audio.muted){
+            audio.muted = false;
+            unmute.style.display = "block";
+            mute.style.display = "none";
+    }})
+    unmute.addEventListener('click', () =>{
+        if(!audio.muted){
+            audio.muted = true;
+            unmute.style.display = "none";
+            mute.style.display = "block";
+    }})
 }
+//VOLUME BAR
+function setVol(el){
+    const aud = document.querySelector('#audio')
+    let newVal = el.value;
+    console.log(newVal)
+    aud.volume = el.value/100;
+    }
 audioPlayer("915785");
